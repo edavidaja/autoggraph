930cc4f32fb5c5c043a9bf0b138aed7d608c6848
edaja <enyinnaya.aja@gmail.com>
2017-03-04 09:53:40 -0500

    eliminated package load on startup

--- app.R
+++ app.R
@@ -1,7 +1,3 @@
-list.of.packages <- c('shiny', 'readr', 'readxl', 'haven', 'ggplot2', 'dplyr', 'stringr','maps')
-new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages)
-
 library(shiny)
 library(readr)
 library(readxl)
