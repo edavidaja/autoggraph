30bafeccb352395bee2b1faa757123fbf6fd5652
edaja <enyinnaya.aja@gmail.com>
2017-03-04 09:52:47 -0500

    reindented code, added text inputs

--- app.R
+++ app.R
@@ -20,41 +20,46 @@ ui <- fluidPage(
   fluidRow(  
     # Sidebar with a slider input for number of bins
     column(3,
-           wellPanel(
+     wellPanel(
              # cascading menu system prompts relationship and then chart
-             uiOutput("select_relationship"),
-             textOutput("desc_text"),
-             br(),
-             uiOutput("select_chart"),
-             
-             fileInput("infile", label = "upload your data (in csv format)"),
-             
-             uiOutput("x_variable"),
-             uiOutput("y_variable"),
-             uiOutput("z_variable"),
-             
-             actionButton("go", "Go!", icon = icon("area-chart"))
-           )
-    ),
+       uiOutput("select_relationship"),
+       textOutput("desc_text"),
+       br(),
+       uiOutput("select_chart"),
+
+       fileInput("infile", label = "upload your data (in csv format)"),
+
+       uiOutput("x_variable"),
+       uiOutput("y_variable"),
+       uiOutput("z_variable"),
+
+       actionButton("go", "Go!", icon = icon("area-chart"))
+       )
+     ),
     column(6, 
            # Show a plot of the generated distribution
-           mainPanel(
-             plotOutput("graph")
-           )
-    ),
+     mainPanel(
+       plotOutput("graph")
+       )
+     ),
     column(3, 
-           wellPanel() 
-    )
-  ),
+     wellPanel(
+      h4("plot labels"),
+      textInput("x_label", "x-axis label"),
+      textInput("y_label", "y-axis label"),
+      textInput("source_label", "source label", placeholder = "Source: GAO analysis of XXX. | GAO-XX-XXX")
+      ) 
+     )
+    ),
   
   hr(),
   fluidRow(
     column(12, 
-           a("Inspired by the FT Visual Vocabulary.",
-             href="https://github.com/ft-interactive/chart-doctor/raw/master/visual-vocabulary/Visual-vocabulary.pdf")
+     a("Inspired by the FT Visual Vocabulary.",
+       href="https://github.com/ft-interactive/chart-doctor/raw/master/visual-vocabulary/Visual-vocabulary.pdf")
+     )
     )
   )
-)
 
 server <- function(input, output, session) {
   # observe({
@@ -66,29 +71,29 @@ server <- function(input, output, session) {
   # Relationship selectors ------------------------------------------------------
   output$select_relationship <- renderUI({
     selectInput("relationship_type", "what kind of relationship are you depicting?",
-                choices = unique(viz_vocab$relationship_type))
+      choices = unique(viz_vocab$relationship_type))
   })
   
   output$desc_text <- renderText({
     req(input$relationship_type)
     
     relationship_description <- viz_vocab %>%
-      filter(relationship_type == input$relationship_type) %>%
-      select(description) %>% unique(.) %>% as.character(.)
+    filter(relationship_type == input$relationship_type) %>%
+    select(description) %>% unique(.) %>% as.character(.)
     
     (relationship_description)
   })
   
   output$select_chart <- renderUI({
-    
+
     req(input$relationship_type)
     
     available_charts <- viz_vocab %>%
-      filter(relationship_type == input$relationship_type) %>%
-      select(chart_type)
+    filter(relationship_type == input$relationship_type) %>%
+    select(chart_type)
     
     selectInput("chart_type", "which chart would you like to use?",
-                choices = available_charts)
+      choices = available_charts)
   })
   
   inserted_scatter <- FALSE
@@ -103,74 +108,74 @@ server <- function(input, output, session) {
     input$chart_type
     input$infile
   },
-               {
+  {
                    # if they ask for a scatter, added the scatter  
-                   if (input$chart_type == 'scatterplot' & inserted_scatter == FALSE)
-                   {
+   if (input$chart_type == 'scatterplot' & inserted_scatter == FALSE)
+   {
                      # have to put some randomness in the id, otherwise shiny doesnt like it
-                     id <<- paste0('smoother', floor(runif(1, min=0, max=101)))
-                     insertUI(
-                       selector = '#placeholder',
-                       ui = tags$div(
-                         selectizeInput('smoother', "choose smoother", c('', 'loess', 'linear', 'quadratic'),
-                                        options = list(
-                                          placeholder = 'Please select an option below',
-                                          onInitialize = I('function() { this.setValue(""); }'))), 
-                         id = id)
-                     )
-                     inserted_scatter <<- TRUE
-  
-                   }
+     id <<- paste0('smoother', floor(runif(1, min=0, max=101)))
+     insertUI(
+       selector = '#placeholder',
+       ui = tags$div(
+         selectizeInput('smoother', "choose smoother", c('', 'loess', 'linear', 'quadratic'),
+          options = list(
+            placeholder = 'Please select an option below',
+            onInitialize = I('function() { this.setValue(""); }'))), 
+         id = id)
+       )
+     inserted_scatter <<- TRUE
+
+   }
                    # once they switch, clear and remove
-                   else if (inserted_scatter == TRUE & input$chart_type != 'scatterplot')
-                   {
-                      updateSelectizeInput(session, inputId = "smoother", "choose smoother", c('', 'loess', 'linear', 'quadratic'),
-                                           options = list(
-                                             placeholder = 'Please select an option below',
-                                             onInitialize = I('function() { this.setValue(""); }')))
-                     removeUI(
-                       selector = paste0('#', id)
-                     )
-                     inserted_scatter <<- FALSE
-                   }
-                   else if (input$chart_type == 'bar'  & inserted_error == FALSE)
-                   {
+   else if (inserted_scatter == TRUE & input$chart_type != 'scatterplot')
+   {
+    updateSelectizeInput(session, inputId = "smoother", "choose smoother", c('', 'loess', 'linear', 'quadratic'),
+     options = list(
+       placeholder = 'Please select an option below',
+       onInitialize = I('function() { this.setValue(""); }')))
+    removeUI(
+     selector = paste0('#', id)
+     )
+    inserted_scatter <<- FALSE
+  }
+  else if (input$chart_type == 'bar'  & inserted_error == FALSE)
+  {
                      # do the same for error bars
-                     limit_id <<- paste0('limit', floor(runif(1, min=0, max=101)))
-                     insertUI(
-                       selector = '#placeholder',
-                       ui = tags$div(
-                         selectizeInput("limit",
-                                        "limit",
-                                        choices = c(names(graph_data()), ''),
-                                        options = list(
-                                          placeholder = 'Please select an option below',
-                                          onInitialize = I('function() { this.setValue(""); }')
-                                        )                     
-                                        ),
-                         id = limit_id)
-                       )
+   limit_id <<- paste0('limit', floor(runif(1, min=0, max=101)))
+   insertUI(
+     selector = '#placeholder',
+     ui = tags$div(
+       selectizeInput("limit",
+        "limit",
+        choices = c(names(graph_data()), ''),
+        options = list(
+          placeholder = 'Please select an option below',
+          onInitialize = I('function() { this.setValue(""); }')
+          )                     
+        ),
+       id = limit_id)
+     )
 
-                     inserted_error <<- TRUE
-                     
-                   }
-                 else if (inserted_error == TRUE & input$chart_type != 'bar')
-                 {
-                   updateSelectizeInput(session, 'limit', choices = c(names(graph_data()), ''), 
-                                        options = list(
-                                          placeholder = 'Please select an option below',
-                                          onInitialize = I('function() { this.setValue(""); }'),
-                                          server = server))
-                   removeUI(
-                     selector = paste0('#', limit_id)
-                   )
-                   inserted_error <<- FALSE
-                   
-                 }
-                 
+   inserted_error <<- TRUE
 
-               }
-  )
+ }
+ else if (inserted_error == TRUE & input$chart_type != 'bar')
+ {
+   updateSelectizeInput(session, 'limit', choices = c(names(graph_data()), ''), 
+    options = list(
+      placeholder = 'Please select an option below',
+      onInitialize = I('function() { this.setValue(""); }'),
+      server = server))
+   removeUI(
+     selector = paste0('#', limit_id)
+     )
+   inserted_error <<- FALSE
+
+ }
+
+
+}
+)
 
   # so i can mess with the assignemt
   values <- reactiveValues()
@@ -179,50 +184,50 @@ server <- function(input, output, session) {
     fil <- input$infile
     values <- reactiveValues(data = NULL)
     values$data <- read.csv(fil$datapath)
-  
+
   })
   
   # Variable selectors ----------------------------------------------------------
   output$x_variable <- renderUI({
-    
+
     selectizeInput("x",
-                   "select your x variable:",
-                   choices =  names(graph_data()),
-                   options = list(
-                     placeholder = 'Please select an option below',
-                     onInitialize = I('function() { this.setValue(""); }')
-                   )
-    )
+     "select your x variable:",
+     choices =  names(graph_data()),
+     options = list(
+       placeholder = 'Please select an option below',
+       onInitialize = I('function() { this.setValue(""); }')
+       )
+     )
   })
   
   output$y_variable <- renderUI({
-    
+
     selectizeInput("y",
-                   "add your y variable:",
-                   choices = c(names(graph_data()), ''),
-                   options = list(
-                     placeholder = 'Please select an option below',
-                     onInitialize = I('function() { this.setValue(""); }')
-                   )
-    )
+     "add your y variable:",
+     choices = c(names(graph_data()), ''),
+     options = list(
+       placeholder = 'Please select an option below',
+       onInitialize = I('function() { this.setValue(""); }')
+       )
+     )
   })
   
   output$z_variable <- renderUI({
-    
+
     selectizeInput("z",
-                   "add your z variable:",
-                   choices = c(names(graph_data()), ''),
-                   options = list(
-                     placeholder = 'Please select an option below',
-                     onInitialize = I('function() { this.setValue(""); }')
-                   )
-    )
+     "add your z variable:",
+     choices = c(names(graph_data()), ''),
+     options = list(
+       placeholder = 'Please select an option below',
+       onInitialize = I('function() { this.setValue(""); }')
+       )
+     )
   })
 
   
   # Graphs ----------------------------------------------------------------
   which_error <- reactive({
-    
+
     verbage1 <- paste(input$y, '+', input$limit) 
     verbage2 <- paste(input$y, '-', input$limit)
     limits <- aes_string(ymax=verbage1, ymin=verbage2)
@@ -231,7 +236,7 @@ server <- function(input, output, session) {
   })
   
   which_smoother <- reactive({
-    
+
     if (input$smoother == 'loess')
     {
       geom_smooth(method='loess')
@@ -246,7 +251,7 @@ server <- function(input, output, session) {
   
   
   which_aes <- reactive({
-    
+
     if (input$x != '' & input$y == '' & input$z == '')
     {
       aes_string(x=input$x)
@@ -279,7 +284,7 @@ server <- function(input, output, session) {
   # added an extra which_geom function for z vars to override the default color
   # changed the these function from switch to if to handle some extra logic
   which_geom_z <- reactive({
-    
+
     if (input$chart_type == 'histogram')
     {
       if (! is.null(graph_data()))
@@ -307,7 +312,7 @@ server <- function(input, output, session) {
     
     else if (input$chart_type == 'map')
     {
-      
+
       geom_map()
     }
     
@@ -315,7 +320,7 @@ server <- function(input, output, session) {
   
   # changed from switch to if to handle extra logic
   which_geom <- reactive({
-    
+
     if (input$chart_type == 'histogram')
     {
       if (! is.null(graph_data()))
@@ -359,7 +364,7 @@ server <- function(input, output, session) {
         axis.title.x = element_text(hjust = 0, size = 7, face = "bold"),
         axis.text = element_text(size = 7, face = "bold"),
         panel.grid = element_blank()
-      )
+        )
       
       # gao custome pallette
       gao_pallete <- c('#99CCFF', '#3F9993', '#044F91', '#330033')
@@ -389,7 +394,7 @@ server <- function(input, output, session) {
         {
           if (input$smoother != '')
           {
-            
+
             p <- p + which_smoother()
           }          
         }
