6bd70d57fd4d677196d0ff6f03d74f5d142d85b7
edaja <enyinnaya.aja@gmail.com>
2017-03-16 21:00:30 -0400

    eliminated conditional panels in preparation for merge to dynamic-ui

--- ui.R
+++ ui.R
@@ -45,20 +45,7 @@ shinyUI(
           ),
        ## conditional panels with plot specific options ------------------------
          wellPanel(
-          h4("plot-specific options:"),
-        ### scatterplots ------------------------------------------------------
-          conditionalPanel("input.chart_type == 'scatterplot'",
-            p("smoother?", id='placeholder')
-            ),
-        ### bar charts --------------------------------------------------------
-          conditionalPanel("input.chart_type == 'bar'",
-            p("error bars?", id='placehold'),
-            p("horizontal?")
-            ),
-        ### line graphs -------------------------------------------------------
-          conditionalPanel("input.chart_type == 'line'",
-            p("linetypes?")
-            )
+          h4("plot-specific options:")
           ),
        ## links to outputs of application -------------------------------------
          wellPanel(
