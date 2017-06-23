141f147c6ce1bdad6355f7232219a82cfa012db7
edaja <enyinnaya.aja@gmail.com>
2017-06-23 16:27:34 -0400

    erased needless copy objects

--- global.R
+++ global.R
@@ -1,16 +1,7 @@
 enableBookmarking(store = "server")
 
 # version number
-app_version <- numeric_version("0.0.0.9005")
-
-# lengthy copy objects
-bar_copy <- list(
-	p("for single variable bar charts (i.e.) histograms, select histogram."),
-	 br(), 
-	 p("for bar charts where your data are already summarized, put the category in x and the summary value in y. put additional categorical variables in z."),
-	 br(),
-	 p("for bar charts with record level data, put the primary category in x, and the second category in z.")
-	)
+app_version <- numeric_version("0.0.0.9006")
 
 # set location of zip for export
 # Sys.setenv(R_ZIPCMD="/usr/bin/zip")
