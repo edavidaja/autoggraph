a52c206ee72beeb2235d0c53d6fdcfaee18a42d7
edaja <enyinnaya.aja@gmail.com>
2017-08-04 09:17:18 -0400

    incremented version and deleted superfluous files from www

--- global.R
+++ global.R
@@ -1,7 +1,4 @@
 enableBookmarking(store = "server")
 
 # version number
-app_version <- numeric_version("0.0.0.9006")
-
-# set location of zip for export
-# Sys.setenv(R_ZIPCMD="/usr/bin/zip")
+app_version <- numeric_version("0.0.0.9007")
